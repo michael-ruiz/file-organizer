6c270086c0a5ac44dbab9273a0aa3eb67bd93f1d
Michael Ruiz <mruiz6@uwo.ca>
2023-08-18 16:23:49 -0400

    program uses multiple folders now

---
+++ README.md
@@ -0,0 +1,3 @@
+# How to use the program
+Windows: run python main.py {name of folder}
+MacOS: run python3 main.py {name of folder}
--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import os
 import shutil
+import sys
 
 # Path to directory
-dir = 'test-file'
+dir = sys.argv[1]
 
 # Create new folders for organizing
-new_folders = ['pdf-files', 'word-files', 'png-files', 'misc']
-for folder in new_folders:
+new_folders = {'p':'pdf-files', 'w':'word-files', 'pn':'png-files', 'm':'misc'}
+for key, folder in new_folders.items():
     f = os.path.join(dir, folder)
     if not os.path.exists(f):
         os.mkdir(f)
 
 for filename in os.listdir(dir):
-    if filename.endswith('.txt'):
+    if filename.endswith('.pdf'):
         old_dir = os.path.join(dir, filename)
-        new_dir = os.path.join(dir, new_folders[3], filename)
+        new_dir = os.path.join(dir, new_folders['p'], filename)
+        shutil.move(old_dir, new_dir)
+    elif filename.endswith('.docx'):
+        old_dir = os.path.join(dir, filename)
+        new_dir = os.path.join(dir, new_folders['w'], filename)
+        shutil.move(old_dir, new_dir)
+    elif filename.endswith('.png'):
+        old_dir = os.path.join(dir, filename)
+        new_dir = os.path.join(dir, new_folders['pn'], filename)
+        shutil.move(old_dir, new_dir)
+    else:
+        old_dir = os.path.join(dir, filename)
+        new_dir = os.path.join(dir, new_folders['m'], filename)
         shutil.move(old_dir, new_dir)
